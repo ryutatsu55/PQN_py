9332aaaf5c03d3f8296a794bcbe1aeda2156bde9
ryutatsu55 <popbreak@icloud.com>
2025-07-18 22:02:49 +0900

    ラスタープロット追加

--- demo.py
+++ demo.py
@@ -9,7 +9,8 @@ from src.Synapses import DoubleExponentialSynapse
 import random
 
 if __name__ == "__main__":
-    seed = 1
+    seed = int(random.random() * 1000)
+    # seed = 2  # random seed for reproducibility
     ##やることリスト
     #学習アルゴリズム追加
 
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     N = int(input("number of neurons: "))
     cell0=PQNModel(mode='RSexci', N = N)
     # set a synapse
-    synapses_out = DoubleExponentialSynapse(N, dt=cell0.PARAM['dt'], td=2e-2, tr=2e-3)
+    synapses_out = DoubleExponentialSynapse(N, dt=cell0.PARAM['dt'], td=1e-2, tr=5e-3)
 
     #initialization
     # length of simulation [s]
@@ -27,7 +28,6 @@ if __name__ == "__main__":
     number_of_iterations=int(tmax/cell0.PARAM['dt'])
 
     v0= np.zeros((number_of_iterations, N))
-    output = np.zeros((number_of_iterations, N))
     spike = np.zeros(N)
     past_spike = np.zeros(N)
     resovoir_weight = np.zeros((N, N))
@@ -38,9 +38,6 @@ if __name__ == "__main__":
         for i in range(int(crust_idx*N/4), int((crust_idx+1)*N/4)):
             for j in range(int(crust_idx*N/4), int((crust_idx+1)*N/4)):
                 p = random.random()
-                if p < 0.5:
-                    i, j = j, i  # 入れ替え
-                p = random.random()
                 if p < 0.05:  # pの確率で結合
                     if j<N/5+crust_idx*N/4:
                         resovoir_weight[i][j] = 1*random.random()
@@ -48,10 +45,13 @@ if __name__ == "__main__":
                         resovoir_weight[i][j] = -1*random.random()  # 2割　抑制結合
         crust_idx += 1
 
-    M = 1   # クラスタ間の接続数
+    M = 4   # クラスタ間の接続数
     for hoge in range(M*4):
         i_range = (hoge*N/4)%N
         j_range = (hoge*N/2)%N
+        p = random.random()
+        if p < 0.5:
+            i, j = j, i  # 入れ替え
         i = random.randint(int(i_range), int(i_range+N/4-1))
         j = random.randint(int((j_range+N/4)%N), int((j_range+N/2-1)%N))
         if (j%(N/4) < N/5):
@@ -65,38 +65,43 @@ if __name__ == "__main__":
     # np.set_printoptions(threshold=np.inf, linewidth=200, suppress=True)
     # print(resovoir_weight)10
     
-    # --- 2. NetworkX グラフに変換 ---
-    G = nx.DiGraph()
-    for i in range(N):
-        for j in range(N):
-            w = resovoir_weight[i][j]
-            if w != 0:
-                G.add_edge(j, i, weight=w)  # j→i（pre→post）
+    # # --- 2. NetworkX グラフに変換 ---
+    # G = nx.DiGraph()
+    # for i in range(N):
+    #     for j in range(N):
+    #         w = resovoir_weight[i][j]
+    #         if w != 0:
+    #             G.add_edge(j, i, weight=w)  # j→i（pre→post）
 
-    # --- 3. ノード配置（円形 or 自動レイアウト） ---
-    pos = nx.spring_layout(G, seed=seed)  # spring_layout / circular_layout など
+    # # --- 3. ノード配置（円形 or 自動レイアウト） ---
+    # pos = nx.spring_layout(G, seed=seed)  # spring_layout / circular_layout など
 
-    # --- 4. エッジ属性（色と太さ） ---
-    edges = G.edges(data=True)
-    edge_colors = ['red' if d['weight'] > 0 else 'blue' for (u, v, d) in edges]
-    edge_widths = [abs(d['weight']) for (u, v, d) in edges]
+    # # --- 4. エッジ属性（色と太さ） ---
+    # edges = G.edges(data=True)
+    # edge_colors = ['red' if d['weight'] > 0 else 'blue' for (u, v, d) in edges]
+    # edge_widths = [abs(d['weight']) for (u, v, d) in edges]
 
-    # --- 5. 可視化 ---
-    plt.figure(num=1, figsize=(8, 8))
-    nx.draw_networkx_nodes(G, pos, node_size=5, node_color="lightgray")
-    nx.draw_networkx_edges(G, pos, edge_color=edge_colors, width=edge_widths, arrows=True, arrowstyle='-|>')
-    plt.title("Reservoir Network Graph")
-    plt.axis('off')
-    plt.tight_layout()
-    plt.savefig("network.png")
-    plt.show(block=False)
+    # # --- 5. 可視化 ---
+    # plt.figure(num=1, figsize=(8, 8))
+    # nx.draw_networkx_nodes(G, pos, node_size=5, node_color="lightgray")
+    # nx.draw_networkx_edges(G, pos, edge_color=edge_colors, width=edge_widths, arrows=True, arrowstyle='-|>')
+    # plt.title("Reservoir Network Graph")
+    # plt.axis('off')
+    # plt.tight_layout()
+    # plt.savefig("network.png")
+    # plt.show(block=False)
 
     # set step input
     dt = cell0.PARAM['dt']
+    delay = np.random.randint(0.03//dt, 0.5//dt)  # delay for each neuron
     I=np.zeros((number_of_iterations, N))
-    I[int(0.05/dt):int(0.1/dt),2] = 0.09
+    I[int(0.05/dt):int(0.1/dt),range(0,N,100)] = 0.09
+    rasters = np.zeros((number_of_iterations, N))
+    output = np.zeros((number_of_iterations+delay, N))
+    cols = np.arange(output.shape[1])
     next_input = np.zeros(N)
-
+    # delays = np.random.randint(0, delay, size=N)  # delay for each neuron
+    delays = np.full(N, delay-1)  # uniform delay for all neurons
     # run simulatiion
     start = time.perf_counter()
     for i in tqdm(range(number_of_iterations)):
@@ -104,18 +109,17 @@ if __name__ == "__main__":
         cell0.update(I[i])  # update cell state
         v0[i] = (cell0.get_membrane_potential())
         spike = np.where(v0[i] > 4, 1, 0)
-        output[i] = synapses_out(np.where(spike-past_spike > 0, 1, 0))
-        # tmp = np.where(spike-past_spike > 0, 1, 0)
-        # if(tmp[1] == 1):
-        #     print(f"Spike at iteration {i}, time {i * cell0.PARAM['dt']:.2f} s")
-        next_input = 0.05*np.dot(resovoir_weight, output[i])  # update input for next iteration
+        rasters[i] = np.where(spike-past_spike > 0, 1, 0)
+        rows = delays + i+1
+        output[rows, cols] = 0.005*synapses_out(rasters[i])  # update output with synapse
+        next_input = np.dot(resovoir_weight, output[i])  # update input for next iteration
         past_spike = spike
     #print(output.shape)
 
 
 
     end = time.perf_counter()
-    print(f"processing time for {tmax*1000}ms simulation mas {(end - start)*1000} ms when reservoir_size was {N}")
+    print(f"processing time for {tmax}s simulation mas {(end - start)} s when reservoir_size was {N}")
 
     # plot simulation result
     fig = plt.figure(num=2, figsize=(10,4))
@@ -123,17 +127,31 @@ if __name__ == "__main__":
     ax0 = fig.add_subplot(spec[0])
     ax1 = fig.add_subplot(spec[1])
     ax2 = fig.add_subplot(spec[2])
-    ax0.plot([i*cell0.PARAM['dt'] for i in range(0, number_of_iterations)], v0[:,4])
+    times, neuron_ids = np.nonzero(rasters)
+    ax0.plot([i*cell0.PARAM['dt'] for i in range(0, number_of_iterations)], v0[:,0])    
     ax0.set_xlim(0, tmax)
     ax0.set_ylabel("v")
     ax0.set_xticks([])
-    ax1.plot([i*cell0.PARAM['dt'] for i in range(0, number_of_iterations)], output[:,4])
+    ax1.plot([i*cell0.PARAM['dt'] for i in range(0, number_of_iterations)], output[:number_of_iterations,0])
     ax1.set_xlim(0, tmax)
     ax1.set_ylabel("output")
     ax1.set_xticks([])
-    ax2.plot([i*cell0.PARAM['dt'] for i in range(0, number_of_iterations)], I[:,4], color="black")
+    ax2.plot([i*cell0.PARAM['dt'] for i in range(0, number_of_iterations)], I[:,0], color="black")
     ax2.set_xlim(0, tmax)
     ax2.set_xlabel("[s]")
     ax2.set_ylabel("I")
-    plt.savefig("demo.png")
+
+    times, neuron_ids = np.nonzero(rasters)
+    times = times * cell0.PARAM['dt']
+    neuron_ids = neuron_ids + 1  # Adjust neuron IDs to start from 1
+    plt.figure(figsize=(9, 2))
+    plt.scatter(times, neuron_ids, s=5, color='black')
+    plt.xlabel("time")
+    plt.xlim(0, tmax)
+    plt.ylabel("neuron ID")
+    plt.ylim(0, N)
+    plt.title("Raster Plot")
+    plt.tight_layout()
     plt.show()
+
+    plt.savefig("demo.png")
